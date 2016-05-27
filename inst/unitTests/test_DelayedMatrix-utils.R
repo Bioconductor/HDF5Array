4974226daa487cdf8e32f75135c413fce727e35f
Herve Pages <hpages@fhcrc.org>
2016-05-27 00:28:52 +0000

    move "cbind" and "rbind" methods to their own file
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/HDF5Array@117915 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_DelayedMatrix-utils.R
+++ inst/unitTests/test_DelayedMatrix-utils.R
@@ -78,69 +78,6 @@ test_DelayedMatrix_Ops <- function()
         test_delayed_Ops_on_matrix(.Generic, m, M)
 }
 
-test_DelayedMatrix_rbind_cbind <- function()
-{
-    m1 <- matrix(1:15, nrow=3, ncol=5,
-                 dimnames=list(NULL, paste0("M1y", 1:5)))
-    m2 <- matrix(101:135, nrow=7, ncol=5,
-                 dimnames=list(paste0("M2x", 1:7), paste0("M2y", 1:5)))
-    m3 <- matrix(1001:1025, nrow=5, ncol=5,
-                 dimnames=list(paste0("M3x", 1:5), NULL))
-    M1 <- HDF5Array(m1)
-    M2 <- HDF5Array(m2)
-    M3 <- HDF5Array(m3)
-
-    target <- rbind(a=m1, b=m2, c=m3)
-    current <- rbind(a=M1, b=M2, c=M3)
-    checkTrue(is(current, "DelayedMatrix"))
-    checkTrue(validObject(current, complete=TRUE))
-    checkIdentical(target, as.matrix(current))
-
-    current <- cbind(a=t(M1), b=t(M2), c=t(M3))
-    checkTrue(is(current, "DelayedMatrix"))
-    checkTrue(validObject(current, complete=TRUE))
-    checkIdentical(t(target), as.matrix(current))
-
-    ## unary form
-
-    target <- rbind(a=m1)
-    current <- rbind(a=M1)
-    checkTrue(is(current, "DelayedMatrix"))
-    checkTrue(validObject(current, complete=TRUE))
-    checkIdentical(target, as.matrix(current))
-
-    target <- cbind(a=m1)
-    current <- cbind(a=M1)
-    checkTrue(is(current, "DelayedMatrix"))
-    checkTrue(validObject(current, complete=TRUE))
-    checkIdentical(target, as.matrix(current))
-
-    ## with empty matrices
-
-    m1 <- matrix(nrow=0, ncol=3, dimnames=list(NULL, letters[1:3]))
-    m2 <- matrix(1:15, ncol=3, dimnames=list(NULL, LETTERS[1:3]))
-    M1 <- HDF5Array(m1)
-    M2 <- HDF5Array(m2)
-
-    target <- rbind(a=m1, a=m2)
-    current <- rbind(a=M1, b=M2)
-    checkTrue(is(current, "DelayedMatrix"))
-    checkTrue(validObject(current, complete=TRUE))
-    checkIdentical(target, as.matrix(current))
-
-    target <- rbind(a=m2, a=m1)
-    current <- rbind(a=M2, b=M1)
-    checkTrue(is(current, "DelayedMatrix"))
-    checkTrue(validObject(current, complete=TRUE))
-    checkIdentical(target, as.matrix(current))
-
-    target <- rbind(a=m1, a=m1)
-    current <- rbind(a=M1, b=M1)
-    checkTrue(is(current, "DelayedMatrix"))
-    checkTrue(validObject(current, complete=TRUE))
-    checkIdentical(target, as.matrix(current))
-}
-
 test_DelayedMatrix_row_col_summary <- function()
 {
     test_row_col_summary <- function(FUN, m, M, block_sizes) {
