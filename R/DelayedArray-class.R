d28db6552ec42f5e5b5a13d0405261efac122b65
Herve Pages <hpages@fhcrc.org>
2016-06-11 02:08:18 +0000

    Fix some issues when the seed of a DelayedArray object is a data.frame or DataFrame object.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/HDF5Array@118471 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/DelayedArray-class.R
+++ R/DelayedArray-class.R
@@ -452,6 +452,62 @@ setMethod("[", "DelayedArray", .extract_DelayedArray_subset)
 ### subset_seed_as_array()
 ###
 
+### Return the slice as a list.
+.extract_data_frame_slice <- function(x, index)
+{
+    slice <- subset_array_like_by_list(x, index)
+    ## Turn into a list and replace factors with character vectors.
+    lapply(slice, as.vector)
+}
+.extract_DataFrame_slice <- function(x, index)
+{
+    slice <- subset_array_like_by_list(x, index)
+    slice <- as.data.frame(slice)
+    ## Turn into a list and replace factors with character vectors.
+    lapply(slice, as.vector)
+}
+
+### Return a list with one list element per column in data frame 'x'.
+### All the list elements have length 0.
+.extract_data_frame_slice0 <- function(x)
+{
+    slice0 <- x[0L, , drop=FALSE]
+    ## Turn into a list and replace factors with character vectors.
+    lapply(slice0, as.vector)
+}
+.extract_DataFrame_slice0 <- function(x)
+{
+    slice0 <- x[0L, , drop=FALSE]
+    slice0 <- as.data.frame(slice0)
+    if (ncol(slice0) != ncol(x))
+        stop(wmsg("DataFrame object 'x' can be used as the seed of ",
+                  "a DelayedArray object only if as.data.frame(x) ",
+                  "preserves the number of columns"))
+    ## Turn into a list and replace factors with character vectors.
+    lapply(slice0, as.vector)
+}
+
+### Equivalent to 'typeof(as.matrix(x))' but with an almost-zero
+### memory footprint (it avoids the cost of turning 'x' into a matrix).
+.get_data_frame_type <- function(x)
+{
+    if (ncol(x) == 0L)
+        return("logical")
+    slice0 <- .extract_data_frame_slice0(x)
+    typeof(unlist(slice0, use.names=FALSE))
+}
+
+### Equivalent to 'typeof(as.matrix(as.data.frame(x)))' but with an
+### almost-zero memory footprint (it avoids the cost of turning 'x' first
+### into a data frame then into a matrix).
+.get_DataFrame_type <- function(x)
+{
+    if (ncol(x) == 0L)
+        return("logical")
+    slice0 <- .extract_DataFrame_slice0(x)
+    typeof(unlist(slice0, use.names=FALSE))
+}
+
 ### 'index' is expected to be an unnamed list of positive integer vectors.
 ### The "subset_seed_as_array" methods don't need to support anything else.
 ### They must return an ordinary array. No need to propagate the dimnames.
@@ -464,41 +520,37 @@ setMethod("subset_seed_as_array", "array",
         subset_array_like_by_list(seed, index)
 )
 
+### Equivalent to
+###
+###     subset_array_like_by_list(as.matrix(x), index)
+###
+### but avoids the cost of turning the full data frame 'x' into a matrix so
+### memory footprint stays small when 'index' is small.
 setMethod("subset_seed_as_array", "data.frame",
     function(seed, index)
     {
-        if (ncol(seed) == 0L) {
-            ans_type <- "logical"
-        } else {
-            slice0 <- seed[0L, , drop=FALSE]
-            ans_type <- typeof(unlist(slice0, use.names=FALSE))
-        }
-
-        slice <- subset_array_like_by_list(seed, index)
-        data <- as.vector(unlist(slice, use.names=FALSE), mode=ans_type)
+        #ans_type <- .get_data_frame_type(seed)
+        slice0 <- .extract_data_frame_slice0(seed)
+        slice <- .extract_data_frame_slice(seed, index)
+        data <- unlist(c(slice0, slice), use.names=FALSE)
         array(data, dim=lengths(index))
     }
 )
 
+### Equivalent to
+###
+###     subset_array_like_by_list(as.matrix(as.data.frame(x)), index)
+###
+### but avoids the cost of turning the full DataFrame 'x' first into a data
+### frame then into a matrix so memory footprint stays small when 'index' is
+### small.
 setMethod("subset_seed_as_array", "DataFrame",
     function(seed, index)
     {
-        ## Compute 'ans_type'.
-        if (ncol(seed) == 0L) {
-            ans_type <- "logical"
-        } else {
-            slice0 <- seed[0L, , drop=FALSE]
-            slice0 <- as.data.frame(slice0)
-            if (ncol(slice0) != ncol(seed))
-                stop(wmsg("DataFrame object 'x' can be used as the seed of ",
-                          "a DelayedArray object only if as.data.frame(x) ",
-                          "preserves the number of columns"))
-            ans_type <- typeof(unlist(slice0, use.names=FALSE))
-        }
-
-        slice <- subset_array_like_by_list(seed, index)
-        slice <- as.data.frame(slice)
-        data <- as.vector(unlist(slice, use.names=FALSE), mode=ans_type)
+        #ans_type <- .get_DataFrame_type(seed)
+        slice0 <- .extract_DataFrame_slice0(seed)
+        slice <- .extract_DataFrame_slice(seed, index)
+        data <- unlist(c(slice0, slice), use.names=FALSE)
         array(data, dim=lengths(index))
     }
 )
@@ -748,6 +800,12 @@ slicing_tip <- c(
 ### S3/S4 combo for as.matrix.DelayedArray
 as.matrix.DelayedArray <- function(x, ...) .from_DelayedArray_to_matrix(x, ...)
 setMethod("as.matrix", "DelayedArray", .from_DelayedArray_to_matrix)
+
+### S3/S4 combo for as.data.frame.DelayedArray
+as.data.frame.DelayedArray <- function(x, row.names=NULL, optional=FALSE, ...)
+    as.data.frame(as.array(x, drop=TRUE),
+                  row.names=row.names, optional=optional, ...)
+setMethod("as.data.frame", "DelayedArray", as.data.frame.DelayedArray)
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- inst/unitTests/test_DelayedArray-class.R
+++ inst/unitTests/test_DelayedArray-class.R
@@ -1,58 +1,141 @@
 test_DelayedArray_constructor <- function()
 {
-    ## 3-col seed
-
-    DF1 <- DataFrame(aa=1:5, bb=letters[1:5], cc=Rle(c(-0.25, 7.1), 3:2))
-    df1 <- as.data.frame(DF1)
-    m1 <- as.matrix(df1)
-
-    check_M1 <- function(M1) {
-        checkTrue(class(M1) == "DelayedMatrix")
-        checkTrue(validObject(M1, complete=TRUE))
-        checkIdentical("character", HDF5Array:::type(M1))
-        checkIdentical("character", HDF5Array:::type(M1[ , -2]))
-        checkIdentical("character", HDF5Array:::type(M1[ , 0]))
-        checkIdentical("character", HDF5Array:::type(M1[0 , ]))
-        checkIdentical(dim(m1), dim(M1))
-        checkIdentical(rownames(m1), rownames(M1))
-        checkIdentical(colnames(m1), colnames(M1))
+    check_DelayedMatrix <- function(m, M) {
+        checkTrue(class(M) == "DelayedMatrix")
+        checkTrue(validObject(M, complete=TRUE))
+        checkIdentical(typeof(m), HDF5Array:::type(M))
+        checkIdentical(typeof(m), HDF5Array:::type(M[ , -2]))
+        checkIdentical(typeof(m), HDF5Array:::type(M[ , 0]))
+        checkIdentical(typeof(m), HDF5Array:::type(M[0 , ]))
+        checkIdentical(dim(m), dim(M))
+        checkIdentical(rownames(m), rownames(M))
+        checkIdentical(colnames(m), colnames(M))
     }
+
+    ## 6-col seed
+
+    DF1 <- DataFrame(aa=1:5,
+                     bb=letters[1:5],
+                     cc=Rle(c(-0.25, 7.1), 3:2),
+                     dd=factor(c("E", "B", "A", "A", "B"), levels=LETTERS),
+                     ee=Rle(factor(c("d", "b"), levels=letters[1:5]), 2:3),
+                     ff=c(FALSE, TRUE, TRUE, FALSE, FALSE))
+
+    df1 <- as.data.frame(DF1)
+    m1 <- as.matrix(df1)      # character matrix
+
     M1a <- DelayedArray(m1)   # matrix seed
-    check_M1(M1a)
+    check_DelayedMatrix(m1, M1a)
+    checkIdentical(m1, as.matrix(M1a))
+    checkIdentical(m1, as.array(M1a))
 
     M1b <- DelayedArray(df1)  # data.frame seed
     rownames(M1b) <- NULL
-    check_M1(M1b)
+    check_DelayedMatrix(m1, M1b)
 
     M1c <- DelayedArray(DF1)  # DataFrame seed
-    check_M1(M1c)
+    check_DelayedMatrix(m1, M1c)
 
-    ## 2-col seed
+    ## 5-col seed
 
-    DF2 <- DF1[ , -2]
+    DF2 <- DF1[ , -2, drop=FALSE]
     df2 <- as.data.frame(DF2)
-    m2 <- as.matrix(df2)
-
-    check_M2 <- function(M2) {
-        checkTrue(class(M2) == "DelayedMatrix")
-        checkTrue(validObject(M2, complete=TRUE))
-        checkIdentical("double", HDF5Array:::type(M2))
-        checkIdentical("double", HDF5Array:::type(M2[ , -2]))
-        checkIdentical("double", HDF5Array:::type(M2[ , 0]))
-        checkIdentical("double", HDF5Array:::type(M2[0 , ]))
-        checkIdentical(dim(m2), dim(M2))
-        checkIdentical(rownames(m2), rownames(M2))
-        checkIdentical(colnames(m2), colnames(M2))
-    }
+    m2 <- as.matrix(df2)      # character matrix
 
     M2a <- DelayedArray(m2)   # matrix seed
-    check_M2(M2a)
+    check_DelayedMatrix(m2, M2a)
+    checkIdentical(m2, as.matrix(M2a))
+    checkIdentical(m2, as.array(M2a))
 
     M2b <- DelayedArray(df2)  # data.frame seed
     rownames(M2b) <- NULL
-    check_M2(M2b)
+    check_DelayedMatrix(m2, M2b)
 
     M2c <- DelayedArray(DF2)  # DataFrame seed
-    check_M2(M2c)
+    check_DelayedMatrix(m2, M2c)
+    
+    ## 4-col seed
+
+    DF3 <- DF1[ , -c(2, 4), drop=FALSE]
+    df3 <- as.data.frame(DF3)
+    m3 <- as.matrix(df3)      # character matrix
+
+    M3a <- DelayedArray(m3)   # matrix seed
+    check_DelayedMatrix(m3, M3a)
+    checkIdentical(m3, as.matrix(M3a))
+    checkIdentical(m3, as.array(M3a))
+
+    M3b <- DelayedArray(df3)  # data.frame seed
+    rownames(M3b) <- NULL
+    check_DelayedMatrix(m3, M3b)
+
+    M3c <- DelayedArray(DF3)  # DataFrame seed
+    check_DelayedMatrix(m3, M3c)
+
+    ## 3-col seed
+
+    DF4 <- DF1[ , -c(2, 4, 5), drop=FALSE]
+    df4 <- as.data.frame(DF4)
+    m4 <- as.matrix(df4)      # double matrix
+
+    M4a <- DelayedArray(m4)   # matrix seed
+    check_DelayedMatrix(m4, M4a)
+    checkIdentical(m4, as.matrix(M4a))
+    checkIdentical(m4, as.array(M4a))
+
+    M4b <- DelayedArray(df4)  # data.frame seed
+    rownames(M4b) <- NULL
+    check_DelayedMatrix(m4, M4b)
+    checkIdentical(m4, as.matrix(M4b))
+    checkIdentical(m4, as.array(M4b))
+
+    M4c <- DelayedArray(DF4)  # DataFrame seed
+    check_DelayedMatrix(m4, M4c)
+    checkIdentical(m4, as.matrix(M4c))
+    checkIdentical(m4, as.array(M4c))
+
+    ## 2-col seed
+
+    DF5 <- DF1[ , c(1, 6), drop=FALSE]
+    df5 <- as.data.frame(DF5)
+    m5 <- as.matrix(df5)      # integer matrix
+
+    M5a <- DelayedArray(m5)   # matrix seed
+    check_DelayedMatrix(m5, M5a)
+    checkIdentical(m5, as.matrix(M5a))
+    checkIdentical(m5, as.array(M5a))
+
+    M5b <- DelayedArray(df5)  # data.frame seed
+    rownames(M5b) <- NULL
+    check_DelayedMatrix(m5, M5b)
+    checkIdentical(m5, as.matrix(M5b))
+    checkIdentical(m5, as.array(M5b))
+
+    M5c <- DelayedArray(DF5)  # DataFrame seed
+    check_DelayedMatrix(m5, M5c)
+    checkIdentical(m5, as.matrix(M5c))
+    checkIdentical(m5, as.array(M5c))
+
+    ## 1-col seed
+
+    DF6 <- DF1[ , 6, drop=FALSE]
+    df6 <- as.data.frame(DF6)
+    m6 <- as.matrix(df6)      # logical matrix
+
+    M6a <- DelayedArray(m6)   # matrix seed
+    check_DelayedMatrix(m6, M6a)
+    checkIdentical(m6, as.matrix(M6a))
+    checkIdentical(m6, as.array(M6a))
+
+    M6b <- DelayedArray(df6)  # data.frame seed
+    rownames(M6b) <- NULL
+    check_DelayedMatrix(m6, M6b)
+    checkIdentical(m6, as.matrix(M6b))
+    checkIdentical(m6, as.array(M6b))
+
+    M6c <- DelayedArray(DF6)  # DataFrame seed
+    check_DelayedMatrix(m6, M6c)
+    checkIdentical(m6, as.matrix(M6c))
+    checkIdentical(m6, as.array(M6c))
 }
 
