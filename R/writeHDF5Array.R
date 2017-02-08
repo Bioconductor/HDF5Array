92904bdce802c1f20b6bc040d29e98ca433e518e
Herve Pages <hpages@fhcrc.org>
2017-02-08 07:39:04 +0000

    some improvements to HDF5RealizationSink() constructor function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/HDF5Array@126585 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/writeHDF5Array.R
+++ R/writeHDF5Array.R
@@ -4,28 +4,36 @@
 ###
 
 
+.check_HDF5_dump_file <- function(file)
+{
+    if (!isSingleString(file))
+        stop(wmsg("'file' must be a single string specifying the path ",
+                  "to an existing HDF5 file or to a new file"))
+    if (file.exists(file))
+        return(h5ls(file))
+    h5createFile(file)
+    return(NULL)
+}
+
+.check_HDF5_dump_name <- function(name)
+{
+    if (!isSingleString(name))
+        stop(wmsg("'name' must be a single string specifying the name ",
+                  "of the HDF5 dataset to write"))
+    if (name == "")
+        stop(wmsg("'name' cannot be the empty string"))
+}
+
 .HDF5_dump_settings_envir <- new.env(parent=emptyenv())
 
 ### Called by .onLoad() hook (see zzz.R file).
 setHDF5DumpFile <- function(file=paste0(tempfile(), ".h5"))
 {
-    if (!isSingleString(file))
-        stop(wmsg("'file' must be a single string specifying the path ",
-                  "to the current output HDF5 file, that is, to the HDF5 ",
-                  "file where all newly created datasets shall be written"))
-    if (file.exists(file)) {
-        show_content <- TRUE
-    } else {
-        h5createFile(file)
-        show_content <- FALSE
-    }
-    ## If h5ls() fails (e.g. file exists but is not HDF5), "file" setting must
-    ## remain untouched.
-    content <- h5ls(file)
+    file_content <- .check_HDF5_dump_file(file)
     assign("file", file, envir=.HDF5_dump_settings_envir)
-    if (show_content)
-        return(content)
-    return(invisible(content))
+    if (is.null(file_content))
+        return(invisible(file_content))
+    return(file_content)
 }
 
 getHDF5DumpFile <- function()
@@ -36,20 +44,23 @@ lsHDF5DumpFile <- function() h5ls(getHDF5DumpFile())
 
 assign("auto_inc_ID", 0L, envir=.HDF5_dump_settings_envir)
 
+.get_auto_inc_ID <- function()
+{
+    get("auto_inc_ID", envir=.HDF5_dump_settings_envir)
+}
+
+.set_HDF5_dump_name_to_next_auto_inc_ID <- function()
+{
+    suppressWarnings(rm(list="name", envir=.HDF5_dump_settings_envir))
+    auto_inc_ID <- .get_auto_inc_ID() + 1L
+    assign("auto_inc_ID", auto_inc_ID, envir=.HDF5_dump_settings_envir)
+}
+
 setHDF5DumpName <- function(name)
 {
-    if (missing(name)) {
-        suppressWarnings(rm(list="name", envir=.HDF5_dump_settings_envir))
-        auto_inc_ID <- get("auto_inc_ID", envir=.HDF5_dump_settings_envir)
-        return(assign("auto_inc_ID", auto_inc_ID + 1L,
-                      envir=.HDF5_dump_settings_envir))
-    }
-    if (!isSingleString(name))
-        stop(wmsg("'name' must be a single string specifying the name of ",
-                  "the next dataset to be written to the current output ",
-                  "HDF5 file"))
-    if (name == "")
-        stop(wmsg("'name' cannot be the empty string"))
+    if (missing(name))
+        return(.set_HDF5_dump_name_to_next_auto_inc_ID())
+    .check_HDF5_dump_name(name)
     assign("name", name, envir=.HDF5_dump_settings_envir)
 }
 
@@ -57,8 +68,8 @@ getHDF5DumpName <- function()
 {
     name <- try(get("name", envir=.HDF5_dump_settings_envir), silent=TRUE)
     if (is(name, "try-error")) {
-        auto_inc_ID <- get("auto_inc_ID", envir=.HDF5_dump_settings_envir)
-        name <- sprintf("/HDF5ArrayDataset%05d", auto_inc_ID)
+        auto_inc_ID <- .get_auto_inc_ID()
+        name <- sprintf("/HDF5ArrayAUTO%05d", auto_inc_ID)
     }
     name
 }
@@ -89,19 +100,25 @@ setMethod("dimnames", "HDF5RealizationSink",
     }
 )
 
-### HDF5RealizationSink object created with an earlier call to
-### HDF5RealizationSink() should be closed before calling HDF5RealizationSink()
-### again.
 ### FIXME: Investigate the possiblity to write the dimnames to the HDF5 file.
-HDF5RealizationSink <- function(dim, dimnames=NULL, type="double")
+HDF5RealizationSink <- function(dim, dimnames=NULL, type="double",
+                                file=NULL, name=NULL)
 {
-    file <- getHDF5DumpFile()
-    name <- getHDF5DumpName()
-    if (is(try(h5createDataset2(file, name, dim, type)), "try-error"))
-        stop(wmsg("Failed to create a new HDF5RealizationSink object. Make ",
-                  "sure to close() the previously created HDF5RealizationSink ",
-                  "object first. Alternatively call setHDF5DumpName() before ",
-                  "trying to call HDF5RealizationSink() again."))
+    if (is.null(file)) {
+        file <- getHDF5DumpFile()
+    } else {
+        .check_HDF5_dump_file(file)
+    }
+    if (is.null(name)) {
+        use_HDF5_dump_name <- TRUE
+        name <- getHDF5DumpName()
+    } else {
+        use_HDF5_dump_name <- FALSE
+        .check_HDF5_dump_name(name)
+    }
+    h5createDataset2(file, name, dim, type)
+    if (use_HDF5_dump_name)
+        .set_HDF5_dump_name_to_next_auto_inc_ID()
     if (is.null(dimnames)) {
         dimnames <- vector("list", length(dim))
     } else {
@@ -126,10 +143,6 @@ setMethod("write_to_sink", c("array", "HDF5RealizationSink"),
         }
         h5write2(x, sink@file, sink@name, index=index)
     }
-)
-
-setMethod("close", "HDF5RealizationSink",
-    function(con, ...) setHDF5DumpName()
 )
 
 
@@ -160,12 +173,8 @@ setAs("HDF5RealizationSink", "HDF5ArraySeed",
 ### possible at the moment. See various FIXMEs above in this file about this.
 writeHDF5Array <- function(x, file, name)
 {
-    old_dump_file <- getHDF5DumpFile()
-    old_dump_name <- getHDF5DumpName()
-    setHDF5DumpFile(file)
-    on.exit({setHDF5DumpFile(old_dump_file); setHDF5DumpName(old_dump_name)})
-    setHDF5DumpName(name)
-    sink <- HDF5RealizationSink(dim(x), dimnames(x), type(x))
+    sink <- HDF5RealizationSink(dim(x), dimnames(x), type(x),
+                                file=file, name=name)
     write_to_sink(x, sink)
     invisible(HDF5Array(as(sink, "HDF5ArraySeed")))
 }
@@ -184,7 +193,6 @@ writeHDF5Dataset <- function(...)
 .dump_as_HDF5ArraySeed <- function(from)
 {
     sink <- HDF5RealizationSink(dim(from), dimnames(from), type(from))
-    on.exit(close(sink))
     write_to_sink(from, sink)
     as(sink, "HDF5ArraySeed")
 }
