81fea5bb8ab4046053ae462cc7e8902829798a0c
Hervé Pagès <hpages@fredhutch.org>
2018-06-13 08:51:05 -0700

    Run full DelayedArray test suite using HDF5Array realization backend
    This is in addition (and is run after) HDF5Array own tests.

--- R/zzz.R
+++ R/zzz.R
@@ -8,5 +8,15 @@
     init_HDF5_dataset_creation_global_counter()
 }
 
-.test <- function() BiocGenerics:::testPackage("HDF5Array")
+.test <- function()
+{
+    BiocGenerics:::testPackage("HDF5Array")
+
+    ## Skip this on 32-bit Windows to avoid 'R CMD check' TIMEOUT on the
+    ## Windows build machines.
+    if (.Platform$OS.type != "windows" || .Platform$r_arch != "i386") {
+        setRealizationBackend("HDF5Array")
+        BiocGenerics:::testPackage("DelayedArray")
+    }
+}
 
