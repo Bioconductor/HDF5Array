9c5914166336a6ba65592cf43bb0aa4b7f9f3bc9
Hervé Pagès <hpages@fredhutch.org>
2018-06-29 03:41:00 -0700

    Another resync with DelayedArray 0.7.17

--- R/dump-management.R
+++ R/dump-management.R
@@ -266,7 +266,7 @@ getHDF5DumpChunkDim <- function(dim, type, ratio=75)
     chunk_len <- as.integer(ceiling(block_maxlen / ratio))
     ## 'block_maxlen' must be a multiple of 'chunk_len'.
     stopifnot(block_maxlen %% chunk_len == 0L)
-    makeCappedVolumeBox(chunk_len, dim, "linear")
+    makeCappedVolumeBox(chunk_len, dim, "first-dim-grows-first")
 }
 
 
