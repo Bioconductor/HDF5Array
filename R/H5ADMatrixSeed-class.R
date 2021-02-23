a2ebcc054ff5605ab9cc7bb0239f306ad8d18216
Hervé Pagès <hpages.on.github@gmail.com>
2021-02-22 16:21:08 -0800

    Improve support for .h5ad files
    * H5ADMatrixSeed() should now be able to find and load the dimnames in
      most cases. Still need to work on the cases that are not covered yet.

--- R/H5ADMatrixSeed-class.R
+++ R/H5ADMatrixSeed-class.R
@@ -6,7 +6,9 @@
 setClass("H5ADMatrixSeed", contains="Array", representation("VIRTUAL"))
 
 setClass("Dense_H5ADMatrixSeed",
-    contains=c("H5ADMatrixSeed", "HDF5ArraySeed")
+    contains=c("H5ADMatrixSeed", "HDF5ArraySeed"),
+    representation(dimnames="list"),
+    prototype(dimnames=list(NULL, NULL))
 )
 setClass("CSC_H5ADMatrixSeed",
     contains=c("H5ADMatrixSeed", "CSC_H5SparseMatrixSeed")
@@ -17,24 +19,30 @@ setClass("CSR_H5ADMatrixSeed",
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### dimnames() method for Dense_H5ADMatrixSeed objects (overwrite method for
+### HDF5Array objects)
+###
+
+setMethod("dimnames", "Dense_H5ADMatrixSeed",
+    function(x) DelayedArray:::simplify_NULL_dimnames(x@dimnames)
+)
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### Transposition
 ###
 
 ### S3/S4 combo for t.CSC_H5ADMatrixSeed
 t.CSC_H5ADMatrixSeed <- function(x)
 {
-    x <- callNextMethod()
-    class(x) <- "CSR_H5ADMatrixSeed"
-    x
+    new2("CSR_H5ADMatrixSeed", callNextMethod())
 }
 setMethod("t", "CSC_H5ADMatrixSeed", t.CSC_H5ADMatrixSeed)
 
 ### S3/S4 combo for t.CSR_H5ADMatrixSeed
 t.CSR_H5ADMatrixSeed <- function(x)
 {
-    x <- callNextMethod()
-    class(x) <- "CSC_H5ADMatrixSeed"
-    x
+    new2("CSC_H5ADMatrixSeed", callNextMethod())
 }
 setMethod("t", "CSR_H5ADMatrixSeed", t.CSR_H5ADMatrixSeed)
 
@@ -42,6 +50,41 @@ setMethod("t", "CSR_H5ADMatrixSeed", t.CSR_H5ADMatrixSeed)
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### Constructor
 ###
+
+### Must return a list of length 2.
+.load_h5ad_dimnames <- function(filepath)
+{
+    ROWNAMES_DATASET <- "/var/_index"
+    COLNAMES_DATASET <- "/obs/_index"
+
+    ok1 <- try(h5isdataset(filepath, ROWNAMES_DATASET), silent=TRUE)
+    ok2 <- try(h5isdataset(filepath, COLNAMES_DATASET), silent=TRUE)
+    if (inherits(ok1, "try-error") || inherits(ok2, "try-error"))
+        stop(wmsg("Cannot read ", filepath, ". Is this a valid HDF5 file?"))
+
+    ans_rownames <- ans_colnames <- NULL
+    if (ok1 || ok2) {
+        if (ok1) {
+            ans_rownames <- h5mread(filepath, ROWNAMES_DATASET)
+        } else {
+            warning(wmsg("could not find rownames (normally stored ",
+                         "in dataset '", ROWNAMES_DATASET, "' ",
+                         "in this .h5ad file"))
+        }
+        if (ok2) {
+            ans_colnames <- h5mread(filepath, COLNAMES_DATASET)
+        } else {
+            warning(wmsg("could not find colnames (normally stored ",
+                         "in dataset '", COLNAMES_DATASET, "' ",
+                         "in this .h5ad file"))
+        }
+    } else {
+        warning(wmsg("could not find dimnames (normally stored ",
+                     "in datasets '", ROWNAMES_DATASET, "' ",
+                     "and '", COLNAMES_DATASET, "') in this .h5ad file"))
+    }
+    list(ans_rownames, ans_colnames)
+}
 
 ### Returns an H5ADMatrixSeed derivative (can be either a Dense_H5ADMatrixSeed,
 ### or a CSC_H5SparseMatrixSeed, or a CSR_H5SparseMatrixSeed object).
@@ -51,6 +94,7 @@ H5ADMatrixSeed <- function(filepath, name="X")
         stop(wmsg("'filepath' must be a single string specifying the ",
                   "path to the h5ad file where the matrix is located"))
     filepath <- file_path_as_absolute(filepath)
+    dimnames <- .load_h5ad_dimnames(filepath)
 
     if (!isSingleString(name))
         stop(wmsg("'name' must be a single string specifying the name of ",
@@ -64,14 +108,14 @@ H5ADMatrixSeed <- function(filepath, name="X")
         if (length(dim(ans0)) != 2L)
             stop(wmsg("Dataset '", name, "' in file '", filepath, "' does ",
                       "not have exactly 2 dimensions."))
-        ans <- new2("Dense_H5ADMatrixSeed", ans0)
+        ans <- new2("Dense_H5ADMatrixSeed", ans0, dimnames=dimnames)
     } else if (h5isgroup(filepath, name)) {
         ans0 <- H5SparseMatrixSeed(filepath, name)
         if (is(ans0, "CSC_H5SparseMatrixSeed"))
             ans_class <- "CSC_H5ADMatrixSeed"
         else
             ans_class <- "CSR_H5ADMatrixSeed"
-        ans <- new2(ans_class, ans0)
+        ans <- new2(ans_class, ans0, dimnames=dimnames)
     } else {
         stop(wmsg("file '", filepath, "' contains no dataset or group ",
                   "named '", name, "'"))
