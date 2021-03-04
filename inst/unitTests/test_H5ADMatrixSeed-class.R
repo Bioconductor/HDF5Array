fab58ac75f622ef07eaed07c9eb21c45d680adf5
Hervé Pagès <hpages.on.github@gmail.com>
2021-03-03 20:56:24 -0800

    Add unit tests for H5ADMatrixSeed objects

---
+++ inst/unitTests/test_H5ADMatrixSeed-class.R
@@ -0,0 +1,65 @@
+library(SingleCellExperiment)
+library(zellkonverter)
+
+.make_TEST_SCE1 <- function(assays.as=NULL)
+{
+    cells <- letters[1:10]
+    genes <- LETTERS[1:20]
+    ncells <- length(cells)
+    ngenes <- length(genes)
+    u <- matrix(rpois(ngenes * ncells, 5),
+                ncol=ncells,
+                dimnames=list(genes, cells))
+    v <- log2(u + 1)    
+    if (!is.null(assays.as)) {
+        u <- as(u, assays.as)
+        v <- as(v, assays.as)
+    }
+    pca <- matrix(runif(ncells*5), ncells)
+    tsne <- matrix(rnorm(ncells*2), ncells)
+    SingleCellExperiment(assays=list(counts=u, logcounts=v),
+                         reducedDims=SimpleList(PCA=pca, tSNE=tsne))
+}
+
+test_Dense_H5ADMatrixSeed <- function()
+{
+    sce <- .make_TEST_SCE1()
+    writeH5AD(sce, "test.h5ad", X_name="counts")
+    on.exit(unlink("test.h5ad"))
+
+    seed <- H5ADMatrixSeed("test.h5ad")
+    checkTrue(is(seed, "Dense_H5ADMatrixSeed"))
+    checkIdentical(dim(sce), dim(seed))
+    checkIdentical(dimnames(sce), dimnames(seed))
+    checkEquals(assay(sce, "counts"), as.array(seed))
+    checkTrue(!is_sparse(seed))
+
+    seed <- H5ADMatrixSeed("test.h5ad", "/layers/logcounts")
+    checkTrue(is(seed, "Dense_H5ADMatrixSeed"))
+    checkIdentical(dim(sce), dim(seed))
+    checkIdentical(dimnames(sce), dimnames(seed))
+    checkEquals(assay(sce, "logcounts"), as.array(seed))
+    checkTrue(!is_sparse(seed))
+}
+
+test_CSR_H5ADMatrixSeed <- function()
+{
+    sce <- .make_TEST_SCE1("dgCMatrix")
+    writeH5AD(sce, "test.h5ad", X_name="counts")
+    on.exit(unlink("test.h5ad"))
+
+    seed <- H5ADMatrixSeed("test.h5ad")
+    checkTrue(is(seed, "CSR_H5ADMatrixSeed"))
+    checkIdentical(dim(sce), dim(seed))
+    checkIdentical(dimnames(sce), dimnames(seed))
+    checkEquals(as.array(assay(sce, "counts")), as.array(seed))
+    checkTrue(is_sparse(seed))
+
+    seed <- H5ADMatrixSeed("test.h5ad", "/layers/logcounts")
+    checkTrue(is(seed, "CSR_H5ADMatrixSeed"))
+    checkIdentical(dim(sce), dim(seed))
+    checkIdentical(dimnames(sce), dimnames(seed))
+    checkEquals(as.array(assay(sce, "logcounts")), as.array(seed))
+    checkTrue(is_sparse(seed))
+}
+
