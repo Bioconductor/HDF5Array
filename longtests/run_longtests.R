1aee703e5b54c1be0bca0d5643944ce34612e0d3
Hervé Pagès <hpages.on.github@gmail.com>
2024-02-13 21:03:31 -0800

    Add DelayedMatrixStats and genefilter to Suggests field
    These are needed for the long tests.

--- longtests/run_longtests.R
+++ longtests/run_longtests.R
@@ -1,4 +1,6 @@
 require("HDF5Array") || stop("unable to load HDF5Array package")
 setAutoRealizationBackend("HDF5Array")
+## The tests in DelayedArray require the DelayedMatrixStats and genefilter
+## packages so HDF5Array must have them in Suggests.
 BiocGenerics:::testPackage("DelayedArray")
 
