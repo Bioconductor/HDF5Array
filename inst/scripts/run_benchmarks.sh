dd491f5c7767c091eb4dc10f786e7471f16dc109
Hervé Pagès <hpages.on.github@gmail.com>
2025-02-11 20:59:00 -0500

    add revised (and finally complete) normalization & PCA timings for rex3

--- inst/scripts/run_benchmarks.sh
+++ inst/scripts/run_benchmarks.sh
@@ -5,8 +5,10 @@
 #   cd path/to/HDF5Array/inst/scripts/timings_dbs/<machine-name>
 #   (time ../../run_benchmarks.sh) >run_benchmarks.log 2>&1 &
 #
-# Takes about 32 hours to complete on xps15 with HDF5Array 1.35.12!
-# Should take between 20 and 50 hours to complete, depending on the machine.
+# Should take between 20 and 55 hours to complete, depending on the machine.
+# With HDF5Array 1.35.12:
+# - takes about 32 hours to complete on xps15
+# - takes about 55 hours to complete on rex3
 #
 set -e  # exit immediately if a simple command exits with a non-zero status
 
