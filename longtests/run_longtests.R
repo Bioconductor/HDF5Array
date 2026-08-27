c4132ae2db816faf0a8c93dd7fccb502aad92e5d
Hervé Pagès <hpages.on.github@gmail.com>
2026-08-26 18:40:22 -0700

    small tweak to long tests

--- longtests/run_longtests.R
+++ longtests/run_longtests.R
@@ -3,4 +3,4 @@ setAutoRealizationBackend("HDF5Array")
 ## The tests in DelayedArray require the DelayedMatrixStats and genefilter
 ## packages so HDF5Array must have them in Suggests.
 BiocGenerics:::testPackage("DelayedArray")
-
+setAutoRealizationBackend()
